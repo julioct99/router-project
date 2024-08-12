3a76ee4e3318bdbf99b2db8621393e1d519e559d
Julio Casallo Tamayo <jcasallo@knowmadmood.com>
2024-08-12 14:09:05 +0200

    Product card basic version

--- src/components/ProductList/ProductCard.tsx
+++ src/components/ProductList/ProductCard.tsx
@@ -1,3 +1,13 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Rating,
+  Typography,
+} from '@mui/material'
+
+import { Link } from 'react-router-dom'
 import { Product } from '../../types/product'
 
 interface ProductCardProps {
@@ -5,7 +15,35 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FunctionComponent<ProductCardProps> = ({ product }) => {
-  return <div>{product.title}</div>
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea LinkComponent={Link} to={`/products/${product.id}`}>
+        <CardMedia
+          component='img'
+          image={product.images[0]}
+          alt='Product image'
+          sx={{ aspectRatio: '1 / 1' }}
+        />
+        <CardContent>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'baseline',
+            }}
+          >
+            <Typography gutterBottom variant='h6'>
+              {product.title}
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              ${product.price}
+            </Typography>
+          </div>
+          <Rating value={product.rating} readOnly size='small' />
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
 }
 
 export default ProductCard
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import './main.css'
+
 import App from './App.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
