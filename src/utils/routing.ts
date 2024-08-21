6a88cd60d9862cf9ddd63e24556039e4f78c2b3a
Julio Casallo Tamayo <jcasallo@knowmadmood.com>
2024-08-21 10:58:08 +0200

    Remove unused utils module

--- src/utils/routing.ts
+++
@@ -1,8 +0,0 @@
-export const getQueryParamsFromUrl = (url: string) => {
-  let queryParams = new URLSearchParams()
-  if (url.includes('?')) {
-    queryParams = new URLSearchParams(url.split('?')[1])
-  }
-
-  return Object.fromEntries(queryParams)
-}
